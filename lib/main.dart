1267557d0f00402cefcf8f42e996a74fb25fee9e
work <temogogitidze@redberry.ge>
2023-07-02 15:29:56 +0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Text('zdarova eee'),
+        ),
+      ),
+    ),
+  );
+}
