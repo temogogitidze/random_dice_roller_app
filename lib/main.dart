6e7b06adae0da00133b2789304f0658ee33dbc94
work <temogogitidze@redberry.ge>
2023-07-02 17:44:07 +0400

    refactor: move custom widget class into separate file and import it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:first_flutter_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello word',
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
-      ),
-    );
-  }
 }
