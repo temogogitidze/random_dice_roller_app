8043a664050b2dba03e15b6662273a41ee054350
work <temogogitidze@redberry.ge>
2023-07-02 17:32:42 +0400

    refactor: move container into custom widget class

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,32 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 26, 2, 80),
-              Color.fromARGB(255, 45, 7, 98)
-            ]),
-          ),
-          child: const Center(
-            child: Text('zdarova akemwdkmadk'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello word',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
