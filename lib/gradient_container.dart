4e824ef00a61c8f56e2738df2ed28b90a8caea98
work <temogogitidze@redberry.ge>
2023-07-03 11:10:15 +0400

    refactor: move statefull widget into separate class

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -7,8 +8,6 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
-
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -20,24 +19,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-                onPressed: rollDice,
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(fontSize: 28),
-                ),
-                child: const Text('Roll dice'))
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
